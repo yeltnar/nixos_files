4f18b8d99c16d4daaf0569c134753d9433288098
Drew Brantley <drew@drew-mbp.lan>
2024-03-07 15:37:51 -0600

    add podman compose

--- configuration.nix
+++ configuration.nix
@@ -138,6 +138,18 @@
     settings.PermitRootLogin = "no";
   };
 
+  virtualisation = {
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+  };
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
