78f1cbdbc4c26c9a9ff90434ba4df66ccb4e16d6
drew <nixos@andbrant.com>
2024-03-08 13:00:32 -0600

    41 current  2024-03-08 13:00:26  23.11.5060.617579a78725  6.1.80                          *

--- configuration.nix
+++ configuration.nix
@@ -215,22 +215,21 @@
     '';
   };
 
-  #systemd.services.git_clone_test = {
-  #  description = "git_clone_test";
-  #  wants = ["basic.target"];
-  #  after = ["basic.target" "network.target"];
-  #  wantedBy = ["multi-user.target"];
-  #  unitConfig = {
-#	conditionalPathExists = "/tmp/time-until";
-#    };
-#    serviceConfig = {
-#      User = "drew";
-#      Group = "drew";
-#      SyslogIdentifier="time-until";
-#      WorkingDirectory="/tmp";
-#      ExecStart="git clone https://github.com/yeltnar/time-until";
-#    };
-#  };
+  systemd.services.git_clone_test = {
+    description = "git_clone_test";
+    wants = ["basic.target"];
+    after = ["basic.target" "network.target"];
+    wantedBy = ["multi-user.target"];
+    unitConfig = {
+	conditionalPathExists = "/tmp/time-until";
+    };
+    serviceConfig = {
+      User = "drew";
+      SyslogIdentifier="time-until";
+      WorkingDirectory="/tmp";
+      ExecStart="/run/current-system/sw/bin/git clone https://github.com/yeltnar/time-until";
+    };
+  };
 
 }
 
