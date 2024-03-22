2c0532a67c97f9f908ae641449c931f93b9498fa
drew <nixos@andbrant.com>
2024-03-22 14:43:52 -0500

    88 current  2024-03-22 14:43:44  23.11.5060.617579a78725  6.1.80                          *

--- includes/systemd-timers-example.nix
+++ includes/systemd-timers-example.nix
@@ -29,6 +29,7 @@
     script = ''
       # env;
       command -v curl
+      curl https://do.andbrant.com
       # mkdir -p /home/drew/cron
       # export bashrc_folder=/home/drew/playin/custom_bashrc;
       # /home/drew/playin/custom_bashrc/bin/ntfy_report_ip >/home/drew/cron/ntfy_report_ip 2>/home/drew/cron/ntfy_report_stderr
