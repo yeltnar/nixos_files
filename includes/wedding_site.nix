a221ef8d1307d0f91ba5f3fc2930e1df74d6353d
drew <nixos@andbrant.com>
2024-03-22 15:29:41 -0500

    95 current  2024-03-22 15:29:33  23.11.5060.617579a78725  6.1.80                          *

--- includes/wedding_site.nix
+++ includes/wedding_site.nix
@@ -35,20 +35,18 @@
     # After = ["network-online.target"];
     # RequiresMountsFor = "/run/user/1000/containers";
 
-    # wants = ["basic.target"];
     requires = ["wedding_site-git-repo.service"];
     after = ["wedding_site-git-repo.service" "podman.service" "podman.socket"];
     wantedBy = ["default.target"];
-    # wantedBy = ["multi-user.target"];
     unitConfig = {
       ConditionPathExists = "/tmp/wedding_site";
     };
+    script = ''
+      podman start 1b04c16fe8c6e6fe62d99666001e4d899057def07f1237a4fed270fd5a52bf04;
+    '';
     serviceConfig = {
       TimeoutStopSec = "70";
       Restart = "on-failure";
-      script = ''
-        podman start 1b04c16fe8c6e6fe62d99666001e4d899057def07f1237a4fed270fd5a52bf04;
-      '';
       # ExecStart = "/run/current-system/sw/bin/podman start 1b04c16fe8c6e6fe62d99666001e4d899057def07f1237a4fed270fd5a52bf04";
       # ExecStop = "/run/current-system/sw/bin/podman stop -t 10 1b04c16fe8c6e6fe62d99666001e4d899057def07f1237a4fed270fd5a52bf04";
       # ExecStopPost = "/run/current-system/sw/bin/podman stop -t 10 1b04c16fe8c6e6fe62d99666001e4d899057def07f1237a4fed270fd5a52bf04";
