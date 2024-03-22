fed70b2afff938952172e2090e7657ac156ce493
drew <nixos@andbrant.com>
2024-03-22 13:48:24 -0500

    82 current  2024-03-22 13:48:17  23.11.5060.617579a78725  6.1.80                          *

--- includes/systemd-timers-example.nix
+++ includes/systemd-timers-example.nix
@@ -8,15 +8,16 @@
     timerConfig = {
       OnBootSec = "5m";
       OnUnitActiveSec = "1m";
-      Unit = "hello-world.service";
+      Unit = "ntfy_report_ip.service";
     };
   };
 
   systemd.services."ntfy_report_ip" = {
     script = ''
-      mkdir -p /home/drew/cron
-      export bashrc_folder=/home/drew/playin/custom_bashrc;
-      /home/drew/playin/custom_bashrc/bin/ntfy_report_ip >/home/drew/cron/ntfy_report_ip 2>/home/drew/cron/ntfy_report_stderr
+      env;
+      # mkdir -p /home/drew/cron
+      # export bashrc_folder=/home/drew/playin/custom_bashrc;
+      # /home/drew/playin/custom_bashrc/bin/ntfy_report_ip >/home/drew/cron/ntfy_report_ip 2>/home/drew/cron/ntfy_report_stderr
     '';
     serviceConfig = {
       Type = "oneshot";
