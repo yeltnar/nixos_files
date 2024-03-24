a54120ad9625e7450134233a0ae5817d565a8231
drew <nixos@andbrant.com>
2024-03-24 17:56:13 -0500

    log podman-compose for wedding site

--- includes/wedding_site.nix
+++ includes/wedding_site.nix
@@ -37,7 +37,7 @@
 
     script = ''
       sleep 120; # sleep so it maybe has the files
-      podman-compose up
+      podman-compose up 2>&1 | tee /tmp/wedding_site/podman-compose.log
     '';
 
     wantedBy = ["multi-user.target"];
