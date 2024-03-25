ff9d4fb3362350cdf79edc80876133e96deb3b5f
drew <nixos@andbrant.com>
2024-03-25 11:44:04 -0500

    125 current  2024-03-25 11:43:56  23.11.5060.617579a78725  6.1.80                          *

--- includes/wedding_site.nix
+++ includes/wedding_site.nix
@@ -37,6 +37,7 @@
 
     script = ''
       # sleep 120; # sleep so it maybe has the files
+      echo $PATH;
       ${pkgs.podman-compose}/bin/podman-compose up 2>&1 | tee /tmp/wedding_site/podman-compose.log
     '';
 
