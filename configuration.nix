0f8a03776f0ba9c7c62a09d9210f407903739e6a
drew <nixos@andbrant.com>
2024-03-27 13:06:07 -0500

    142 current  2024-03-27 13:05:56  23.11.5060.617579a78725  6.1.80                          *

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,7 @@
     ./includes/dot_bashrc.nix
     ./includes/wedding_site.nix
     ./includes/vaultwarden.nix
+    ./includes/caddy.nix
     ./includes/yeltnar_dev.nix
     ./includes/systemd-timers-example.nix
   ];
