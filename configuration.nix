b16451f8215611d19bd339d48b0ecf7bbb9b7110
Drew Brantley <github@andbrant.com>
2024-05-12 15:26:47 -0500

    47 current  2024-05-12 15:26:42  23.11.4976.79baff8812a0  6.1.79                          *

--- configuration.nix
+++ configuration.nix
@@ -142,6 +142,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    home-manager
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     curl
