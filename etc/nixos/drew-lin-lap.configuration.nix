1b88f926035419117226c04e17c80cee9ee0878b
Drew Brantley <github@andbrant.com>
2024-05-13 12:31:27 -0500

    add drew-lin-lap configuration

---
+++ etc/nixos/drew-lin-lap.configuration.nix
@@ -0,0 +1,19 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+{
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    /etc/nixos/hardware-configuration.nix
+    /home/drew/playin/nixos_files/generic_configuration.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "drew-lin-lap"; # Define your hostname.
+}
