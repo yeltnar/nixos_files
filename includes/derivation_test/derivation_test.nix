51e83ae202e502e89ee2b0e368366fed8ded2c90
Drew Brantley <github@andbrant.com>
2024-05-16 09:55:15 -0500

    add derivation_test.nix

---
+++ includes/derivation_test/derivation_test.nix
@@ -0,0 +1,17 @@
+{pkgs ? import <nixpkgs> {} }:
+
+pkgs.stdenv.mkDerivation {
+
+  name ="derivation_test";
+  src = ./src;
+
+  buildPhase = ''
+    whoami;
+    echo 'date' > r.sh;
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin;
+    cp r.sh $out/bin/;
+  '';
+}
