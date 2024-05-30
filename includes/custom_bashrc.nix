b9785dcb4dfaf1421ed9cad5975b08b25496fc66
Drew Brantley <github@andbrant.com>
2024-05-30 13:12:53 -0500

    move custom bashrc setup

--- includes/custom_bashrc.nix
+++ includes/custom_bashrc.nix
@@ -19,4 +19,29 @@
       ExecStart = "/run/current-system/sw/bin/git clone https://github.com/yeltnar/custom_bashrc";
     };
   };
+
+  system.activationScripts.setup_bash_profile = {
+    text = ''
+      date > /tmp/testing_info;
+
+      text_to_add='if [ -f ~/.bashrc ]; then . ~/.bashrc; fi'
+      text_to_check='# bashrc_load_done # text showing the .bashrc loading is added to profile';
+
+      # create if not there
+      if [ ! -e /home/drew/.bash_profile ]; then
+        touch /home/drew/.bash_profile;
+      fi
+
+      test_str=$(cat /home/drew/.bash_profile | grep "$text_to_check");
+      # echo $text_to_check;
+      # echo $test_str;
+
+      if [ -z "$test_str" ]; then
+        echo "$text_to_check" >> /home/drew/.bash_profile;
+        echo "$text_to_add" >> /home/drew/.bash_profile;
+      fi
+
+      date > /tmp/testing_info_2;
+    '';
+  };
 }
