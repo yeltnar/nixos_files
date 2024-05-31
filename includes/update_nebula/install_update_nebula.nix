df7122ff5ffc30f5e82b5381b7747ae75afc5e31
Drew Brantley <github@andbrant.com>
2024-05-31 14:26:44 -0500

    vars for install_update_nebula

--- includes/update_nebula/install_update_nebula.nix
+++ includes/update_nebula/install_update_nebula.nix
@@ -6,17 +6,19 @@
   cloned_repo = builtins.fetchGit {
     url = "https://github.com/yeltnar/nebula-ansible";
   };
+  vardir = "/var/yeltnar-nebula";
+  user = "drew";
 in {
   system.activationScripts.setup_nebula_env = {
     text = ''
       text_to_check="# setup nebula env # ";
 
-      dir_to_add="/home/drew/.config/extra_includes";
+      dir_to_add="/home/${user}/.config/extra_includes";
       file_to_add="$dir_to_add/setup_nebula_env";
       text_to_add="source $file_to_add"; # TODO remove
 
       mkdir -p "$dir_to_add";
-      chown -R drew:100 "$dir_to_add";
+      chown -R ${user}:100 "$dir_to_add";
 
       # create sourced file if not there
       cat > $file_to_add <<- EOM
@@ -30,60 +32,59 @@ in {
 
 
         # alert missing files
-        if [ ! -e /var/yeltnar-nebula/.env ]; then
-          echo "!! /var/yeltnar-nebula/.env is not present";
+        if [ ! -e ${vardir}/.env ]; then
+          echo "!! ${vardir}/.env is not present";
         fi
-        if [ ! -e /var/yeltnar-nebula/knownca.pem ]; then
-          echo "!! /var/yeltnar-nebula/knownca.pem is not present";
+        if [ ! -e ${vardir}/knownca.pem ]; then
+          echo "!! ${vardir}/knownca.pem is not present";
         fi
-        if [ ! -e /var/yeltnar-nebula/id_rsa ]; then
-          echo "!! /var/yeltnar-nebula/id_rsa is not present";
+        if [ ! -e ${vardir}/id_rsa ]; then
+          echo "!! ${vardir}/id_rsa is not present";
         fi
-        if [ ! -e /var/yeltnar-nebula/id_rsa.pub ]; then
-          echo "!! /var/yeltnar-nebula/id_rsa.pub is not present";
+        if [ ! -e ${vardir}/id_rsa.pub ]; then
+          echo "!! ${vardir}/id_rsa.pub is not present";
         fi
       EOM
 
-      chown drew:100 "$file_to_add";
+      chown ${user}:100 "$file_to_add";
 
       # create bashrc file if not there
-      if [ ! -e /home/drew/.bashrc ]; then
-        touch /home/drew/.bashrc;
-        chown drew /home/drew/.bashrc;
+      if [ ! -e /home/${user}/.bashrc ]; then
+        touch /home/${user}/.bashrc;
+        chown ${user} /home/${user}/.bashrc;
       fi
 
       # create update_nebula dir if not there
-      if [ ! -d /var/yeltnar-nebula ]; then
-        mkdir -p /var/yeltnar-nebula
-        chown drew /var/yeltnar-nebula;
+      if [ ! -d ${vardir} ]; then
+        mkdir -p ${vardir}
+        chown ${user} ${vardir};
       fi
 
       # create compare_date.sh file if not there
-      if [ ! -e /var/yeltnar-nebula/compare_date.sh ]; then
-        cp ${cloned_repo.outPath}/compare_date.sh /var/yeltnar-nebula/compare_date.sh;
-        chown drew:100 /var/yeltnar-nebula/compare_date.sh;
+      if [ ! -e ${vardir}/compare_date.sh ]; then
+        cp ${cloned_repo.outPath}/compare_date.sh ${vardir}/compare_date.sh;
+        chown ${user}:100 ${vardir}/compare_date.sh;
       fi
       # create process_tar.sh file if not there
-      if [ ! -e /var/yeltnar-nebula/process_tar.sh ]; then
-        cp ${cloned_repo.outPath}/process_tar.sh /var/yeltnar-nebula/process_tar.sh;
-        chown drew:100 /var/yeltnar-nebula/process_tar.sh;
+      if [ ! -e ${vardir}/process_tar.sh ]; then
+        cp ${cloned_repo.outPath}/process_tar.sh ${vardir}/process_tar.sh;
+        chown ${user}:100 ${vardir}/process_tar.sh;
       fi
 
-      # TODO # create id_rsa(.pub) if theyre not there;
-      if [ ! -e /var/yeltnar-nebula/id_rsa ]; then
-        pushd /var/yeltnar-nebula;
+      if [ ! -e ${vardir}/id_rsa ]; then
+        pushd ${vardir};
         # TODO # create id_rsa(.pub) if theyre not there;
-        ${pkgs.openssh}/bin/ssh-keygen -t rsa -m PEM -N "" -q -f "/var/yeltnar-nebula/id_rsa" -b 4096;
+        ${pkgs.openssh}/bin/ssh-keygen -t rsa -m PEM -N "" -q -f "${vardir}/id_rsa" -b 4096;
         popd;
       fi
 
-      test_str=$(cat /home/drew/.bashrc | grep "$text_to_check");
+      test_str=$(cat /home/${user}/.bashrc | grep "$text_to_check");
       # echo $text_to_check;
       # echo $test_str;
 
       if [ -z "$test_str" ]; then
-        echo "$text_to_check" >> /home/drew/.bashrc;
-        echo "$text_to_add" >> /home/drew/.bashrc;
+        echo "$text_to_check" >> /home/${user}/.bashrc;
+        echo "$text_to_add" >> /home/${user}/.bashrc;
       fi
     '';
   };
