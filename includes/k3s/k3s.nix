c00cb66a17c2bfb0036024b175f37d2f26bd3e92
Drew Brantley <drew@drew-mbp.lan>
2024-07-31 13:27:43 -0500

    notes in k3s.nix

--- includes/k3s/k3s.nix
+++ includes/k3s/k3s.nix
@@ -1,3 +1,9 @@
+# docs
+# https://search.nixos.org/options?channel=unstable&show=services.k3s.serverAddr&from=0&size=50&sort=relevance&type=packages&query=k3s
+# https://github.com/NixOS/nixpkgs/blob/nixos-unstable/nixos/modules/services/cluster/k3s/default.nix
+
+# to clean, use `sudo rm -rf /var/lib/rancher ; sudo rm -rf /etc/rancher/`
+
 # considering linking `/etc/rancher/k3s/k3s.yaml` -> `/home/drew/.kube/config`
 # (and `chmod +x ...`)
 
@@ -7,7 +13,10 @@
   ...
 }:
 let
+  # token can be found on the server (master) at `/var/lib/rancher/k3s/server/node-token` 
   token = "";
+  # TODO change token to tokenFile
+
   serverAddr = ""; # "https://<ip of first node>:6443";
   role = "agent"; # defaults to agent # one of "server" or "agent" for worker only nodes
 
