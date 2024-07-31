b500dbc244c3a7c0ca1ce8a42fb1035511c16bf5
Drew Brantley <drew@drew-mbp.lan>
2024-07-31 14:08:31 -0500

    k3s default to closed port for api server

--- includes/k3s/k3s.nix
+++ includes/k3s/k3s.nix
@@ -44,7 +44,7 @@ in
 {
 
   networking.firewall.allowedTCPPorts = [
-    6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+    # 6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
     # 2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
     # 2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
   ];
