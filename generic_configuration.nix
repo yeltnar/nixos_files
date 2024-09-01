2441ff40dc8fe530df1596a7f440c09554764ea1
Drew Brantley <github@andbrant.com>
2024-09-01 15:42:36 -0500

    add distrobox to generic_configuration

--- generic_configuration.nix
+++ generic_configuration.nix
@@ -124,6 +124,8 @@
     
     home-manager
     dconf2nix
+
+    distrobox
     
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
