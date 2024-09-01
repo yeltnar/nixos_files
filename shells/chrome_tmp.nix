25c09603432960c31c862a93cc2fecff21a1b098
Drew Brantley <github@andbrant.com>
2024-09-01 18:40:41 -0500

    TODO for chrome distrobox

--- shells/chrome_tmp.nix
+++ shells/chrome_tmp.nix
@@ -5,6 +5,8 @@ mkShell {
     google-chrome
   ];
   shellHook = ''
+  	# TODO download this deb in distrobox, and install 
+  	# https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   	google-chrome-stable --user-data-dir=/tmp/chrome-user-data; exit; 
   '';
 }
