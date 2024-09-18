8ddad6e101960fbb05ae486f2d26fd6452a2a362
drew <github@andbrant.com>
2024-09-18 15:11:00 -0500

    add vm.nix

---
+++ includes/vm/vm.nix
@@ -0,0 +1,27 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  programs.dconf.enable = true;
+
+  users.users.gcis.isSystemUser = true;
+  users.groups.gcis = {};
+  users.users.gcis.group = "gcis";
+  users.users.gcis.extraGroups = ["libvirtd" "kvm"];
+
+  environment.systemPackages = with pkgs; [virt-manager virt-viewer spice spice-gtk spice-protocol win-virtio win-spice gnome.adwaita-icon-theme];
+
+  virtualisation = {
+    libvirtd = {
+      enable = true;
+      qemu = {
+        swtpm.enable = true;
+        ovmf.enable = true;
+        ovmf.packages = [pkgs.OVMFFull.fd];
+      };
+    };
+    spiceUSBRedirection.enable = true;
+  };
+  services.spice-vdagentd.enable = true;
+}
