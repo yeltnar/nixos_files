f2b676454dcabf3c711fb1ecc4a9a5bc01caeeff
drew <nixosvm@andbrant.com>
2024-10-09 13:32:58 -0500

    add back chrom nix import

--- shells/chrome_tmp.nix
+++ shells/chrome_tmp.nix
@@ -1,3 +1,6 @@
+with (import <nixpkgs> { 
+    config.allowUnfree = true;
+});
 let
   unstable = import
     (builtins.fetchTarball https://github.com/nixos/nixpkgs/tarball/nixpkgs-unstable) { config.allowUnfree = true; };
