a5b4db7d597ab61232769037c4347b09f31c0376
drew <github@andbrant.com>
2024-10-11 15:52:31 -0500

    full nbdkit shell example

--- shells/nbdkit.nix
+++ shells/nbdkit.nix
@@ -9,9 +9,19 @@ mkShell {
   ];
   shellHook = ''
   	# nbd-server 9999 /tmp/_rm/to_squash.sqsh; 
-	nbdkit memory 1G
-	sudo nbd-client localhost /dev/nbd14;
+	nbdkit --exit-with-parent memory 10G & # easy cleanup 
+	server_pid="$!"  
+	sudo nbd-client localhost /dev/nbd0;
+        sudo mkfs.btrfs /dev/nbd0
+	mkdir -p /tmp/nbd_btrfs
+	sudo mount /dev/nbd0 /tmp/nbd_btrfs
+	sudo chown drew:100 /tmp/nbd_btrfs
+	echo "saved pid is $server_pid";
 	bash;
+	sudo umount /tmp/nbd_btrfs
+	sudo nbd-client -d /dev/nbd0;
+	echo " killing $server_pid";
+	kill -9 "$server_pid";
 	exit;
   '';
 }
