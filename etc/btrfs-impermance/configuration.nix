58f5925bc8b467e4e32248fd9cc63ea35d460625
a <a>
2024-10-15 15:34:18 -0500

    add lazygit

--- etc/btrfs-impermance/configuration.nix
+++ etc/btrfs-impermance/configuration.nix
@@ -108,6 +108,7 @@ fileSystems = {
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   git
+  lazygit
   ];
 
   programs.neovim = {
