4d5c9fedd913505823d02633d8c688c8b4e36ead
a <a>
2024-10-15 15:18:18 -0500

    make home not persist; add some pkgs

--- etc/btrfs-impermance/configuration.nix
+++ etc/btrfs-impermance/configuration.nix
@@ -107,7 +107,15 @@ fileSystems = {
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
+  git
   ];
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+  }; 
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
