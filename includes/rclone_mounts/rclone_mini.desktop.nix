45230d85b021850a18bf86d84606e90bad3654b2
drew <github@andbrant.com>
2024-12-20 13:54:11 -0600

    move rclone to create dir as root, and change owner to set user, and run as that user

--- includes/rclone_mounts/rclone_mini.desktop.nix
+++ includes/rclone_mounts/rclone_mini.desktop.nix
@@ -7,14 +7,13 @@
   user = "drew";
   port = "22";
   remote_dir = "/";
-  local_dir = "/tmp/mini";
+  local_dir = "/mnt/mini";
   private_key = "/home/drew/.ssh/id_ed25519";
   pub_cert = "/home/drew/.ssh/id_ed25519-cert.pub";
 in {
 
 ### Add "rclone" to your packages first
 
-# RClone Google Drive service
 systemd.services.rclone-mini-mount = {
   # Ensure the service starts after the network is up
   wantedBy = [ "multi-user.target" ];
@@ -24,13 +23,13 @@ systemd.services.rclone-mini-mount = {
   # Service configuration
   serviceConfig = {
     Type = "simple";
-    ExecStartPre = "/run/current-system/sw/bin/mkdir -p ${local_dir}"; # Creates folder if didn't exist
-    ExecStart = "${pkgs.rclone}/bin/rclone mount --vfs-cache-mode full --allow-other --sftp-key-file ${private_key} --sftp-pubkey-file ${pub_cert} --sftp-host ${host} --sftp-user ${user} --sftp-port ${port} :sftp:${remote_dir} ${local_dir}";
+    ExecStartPre = "+${pkgs.bash}/bin/bash -c '/run/current-system/sw/bin/mkdir -p ${local_dir}; ${pkgs.coreutils}/bin/chown ${user} ${local_dir} '"; # Creates folder if didn't exist
+    ExecStart = "${pkgs.util-linux}/bin/runuser -u drew -- ${pkgs.bash}/bin/bash -c '${pkgs.rclone}/bin/rclone mount --vfs-cache-mode full --allow-other --sftp-key-file ${private_key} --sftp-pubkey-file ${pub_cert} --sftp-host ${host} --sftp-user ${user} --sftp-port ${port} :sftp:${remote_dir} ${local_dir}'";
     ExecStop = "/run/current-system/sw/bin/fusermount -u ${local_dir}"; # Dismounts
     Restart = "on-failure";
     RestartSec = "10s";
-    User = "drew";
-    Group = "users";
+    # User = "drew";
+    # Group = "users";
     Environment = [ "PATH=/run/wrappers/bin/:$PATH" ]; # Required environments
   };
 };
