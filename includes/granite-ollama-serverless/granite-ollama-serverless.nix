14153dcc6f8a2302b0c6e4c3aad38de979f4517b
drew <github@andbrant.com>
2025-01-14 12:26:39 -0600

    change directory name; fix up signals from containers

--- includes/granite-ollama-serverless/granite-ollama-serverless.nix
+++ includes/granite-ollama-serverless/granite-ollama-serverless.nix
@@ -14,7 +14,7 @@
   # enable lingering so service starts before user logs in
   users.users.drew.linger = true;
 
-  systemd.sockets.systemd-proxy_serverless = {
+  systemd.sockets.granite-ollama_serverless = {
     requires = [
       "network-online.target"
       "default.target"
@@ -29,13 +29,48 @@
     ];
   };
 
-  systemd.services.systemd-proxy_serverless = {
+  systemd.services.granite-ollama_serverless = {
     serviceConfig = { 
       ExecStart = "${pkgs.systemd}/lib/systemd/systemd-socket-proxyd --exit-idle-time=10s 127.0.0.1:8443";
     };
   };
 
-  systemd.user.sockets.systemd-proxy_serverless = {
+  # serverless socket setup dedicated to ollama
+  systemd.user.sockets.granite-ollama_small_serverless = {
+    requires = [
+      # "network-online.target"
+      "default.target"
+    ]; # TODO make sure this is there, if starting at boot 
+    wantedBy = [
+      "default.target"
+      "sockets.target"
+      "multi-user.target"
+    ];
+    listenStreams = [
+      # "127.0.0.1:8443"
+      "11435"
+      # "192.168.2.180:9999"
+      # "8080"
+    ];
+  };
+
+  systemd.user.services.granite-ollama_small_serverless = {
+    requires = [
+      "granite-ollama_start_ollama.service"
+      "granite-ollama_small_serverless.socket"
+    ];
+    after = [
+      "granite-ollama_start_ollama.service"
+      "granite-ollama_small_serverless.socket"
+    ];
+
+    serviceConfig = { 
+      ExecStart = "${pkgs.systemd}/lib/systemd/systemd-socket-proxyd --exit-idle-time=180s 127.0.0.1:11434";
+    };
+  };
+
+  # serverless socket setup for open-webui, and ollama
+  systemd.user.sockets.granite-ollama_serverless = {
     requires = [
       # "network-online.target"
       "default.target"
@@ -53,16 +88,16 @@
     ];
   };
 
-  systemd.user.services.systemd-proxy_serverless = {
+  systemd.user.services.granite-ollama_serverless = {
     requires = [
-      "systemd-proxy_start_ollama.service"
-      "systemd-proxy_start_open-webui.service"
-      "systemd-proxy_serverless.socket"
+      "granite-ollama_start_ollama.service"
+      "granite-ollama_start_open-webui.service"
+      "granite-ollama_serverless.socket"
     ];
     after = [
-      "systemd-proxy_start_ollama.service"
-      "systemd-proxy_start_open-webui.service"
-      "systemd-proxy_serverless.socket"
+      "granite-ollama_start_ollama.service"
+      "granite-ollama_start_open-webui.service"
+      "granite-ollama_serverless.socket"
     ];
 
     serviceConfig = { 
@@ -70,11 +105,11 @@
     };
   };
 
-  systemd.services.systemd-proxy-git-repo = {
+  systemd.services.granite-ollama-git-repo = {
     path = with pkgs; [
       git
     ];
-    description = "systemd-proxy-git-repo";
+    description = "granite-ollama-git-repo";
     wants = ["basic.target"];
     requires = ["network-online.target"];
     after = ["basic.target" "network-online.target"];
@@ -87,9 +122,9 @@
     '';
     serviceConfig = {
       Type = "oneshot";
-      SyslogIdentifier = "systemd-proxy";
+      SyslogIdentifier = "granite-ollama";
       WorkingDirectory = "/tmp";
-      # ExecStartPost = "systemctl start systemd-proxy_start.service";
+      # ExecStartPost = "systemctl start granite-ollama_start.service";
     };
   };
 
@@ -97,7 +132,7 @@
     DefaultEnvironment="PATH=/run/current-system/sw/bin"
   '';
 
-  systemd.user.services.systemd-proxy_start_ollama = {
+  systemd.user.services.granite-ollama_start_ollama = {
     path = with pkgs; [
       podman
       podman-compose
@@ -106,11 +141,13 @@
     # WARNING this process can not self re-start, or it will confuse the serverless aspect
     script = ''
       PATH="$PATH:${pkgs.podman}/bin";
+      ${pkgs.podman-compose}/bin/podman-compose down ollama
       ${pkgs.podman-compose}/bin/podman-compose --podman-run-args="--replace --sdnotify=container --pidfile=/tmp/systemd_proxy_ollama_podman.pid --replace" up --no-recreate -d ollama
-      
-      podman-compose logs -f open-webui 2>&1 | while IFS= read -r line; do
+
+
+      podman-compose logs -f ollama 2>&1 | while IFS= read -r line; do
         # Process the line
-        if [[ "$line" == *"Uvicorn running on"* ]]; then
+        if [[ "$line" == *"Listening on"* ]]; then
           echo "Found a match: $line"
           # Take action, e.g., run another command
           systemd-notify --ready --status="container up"
@@ -133,17 +170,17 @@
       WorkingDirectory = "/tmp/granite-ollama"; # TODO change repo location
       Restart = "always";
       NotifyAccess = "all";
-      # PIDFile = "/tmp/systemd_proxy_ollama_podman.pid"; # TODO change pid location 
+      PIDFile = "/tmp/systemd_proxy_ollama_podman.pid"; # TODO change pid location 
     };
   };
 
   # TODO create exit scripts for containers 
-  # pkgs.writeShellScript = "stop-systemd-proxy_start_open-webui" ''
+  # pkgs.writeShellScript = "stop-granite-ollama_start_open-webui" ''
   #   PATH="$PATH:${pkgs.podman}/bin";
   #   ${pkgs.podman-compose}/bin/podman-compose --podman-run-args="--replace --sdnotify=container --pidfile=/tmp/systemd_proxy_open-webui_podman.pid --replace" down --no-recreate -d open-webui
   # '';
 
-  systemd.user.services.systemd-proxy_start_open-webui = {
+  systemd.user.services.granite-ollama_start_open-webui = {
     path = with pkgs; [
       podman
       podman-compose
@@ -152,7 +189,20 @@
     # WARNING this process can not self re-start, or it will confuse the serverless aspect
     script = ''
       PATH="$PATH:${pkgs.podman}/bin";
+      ${pkgs.podman-compose}/bin/podman-compose down open-webui 
       ${pkgs.podman-compose}/bin/podman-compose --podman-run-args="--replace --sdnotify=container --pidfile=/tmp/systemd_proxy_open-webui_podman.pid --replace" up --no-recreate -d open-webui
+
+      podman-compose logs -f open-webui 2>&1 | while IFS= read -r line; do
+        # Process the line
+        if [[ "$line" == *"Uvicorn running on"* ]]; then
+          echo "Found a match: $line"
+          # Take action, e.g., run another command
+          systemd-notify --ready --status="container up"
+        else
+          echo "x-$line"
+        fi
+      done
+ 
     '';
     # wantedBy = ["multi-user.target"];
     unitConfig = {
@@ -164,7 +214,7 @@
     };
     serviceConfig = {
       # Type = "simple";
-      ExecStop = ""; # TODO 
+      # ExecStop = ""; # TODO 
       WorkingDirectory = "/tmp/granite-ollama"; # TODO change repo location
       Restart = "always";
       NotifyAccess = "all";
