05326419c0245718b6e4cd8944ae33f42e77b9a2
Drew Brantley <drew@Mac.lan>
2025-03-07 10:36:39 -0600

    generate flakes for multiple arch

--- shells/chrome/flake.nix
+++ shells/chrome/flake.nix
@@ -5,22 +5,29 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs } @ args:
-  let
-    pkgs = import nixpkgs {
-      inherit system;
-      config = {
-        allowUnfree = true;
-      };
-    };
-    system = "x86_64-linux";
-    config = self.config;
-  in{
-    devShells."${system}".default = pkgs.mkShell {
-      packages = with pkgs; [  google-chrome ];
-      shellHook= ''
-  	    google-chrome-stable --user-data-dir=/tmp/chrome-user-data; exit; 
-      '';
-    };
+  outputs = { self, nixpkgs } @ args: { 
+    devShells = builtins.listToAttrs ( 
+      map ( system: {
+        name = "${system}";
+        value = (
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              config = {
+                allowUnfree = true;
+              };
+            };
+            # config = self.config;
+          in {
+            default = pkgs.mkShell {
+              packages = with pkgs; [  google-chrome ];
+              shellHook= ''
+                google-chrome-stable --user-data-dir=/tmp/chrome-user-data; exit; 
+              '';
+            };
+          }
+        );
+      }) ["x86_64-linux" "aarch64-darwin"]
+    );
   };
 }
