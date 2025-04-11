4bb026d561ab3f1155c0b01fcb15f9e675ab95c1
drew <github@andbrant.com>
2025-04-11 09:51:13 -0500

    add kdenlive shell

---
+++ shells/kdenlive/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "kdenlive";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs } @ args: { 
+    devShells = builtins.listToAttrs ( 
+      map ( system: {
+        name = "${system}";
+        value = (
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              config = {
+                allowUnfree = true;
+              };
+            };
+            # config = self.config;
+          in {
+            default = pkgs.mkShell {
+              packages = with pkgs; [ kdenlive ];
+              shellHook= ''
+                kdenlive; exit; 
+              '';
+            };
+          }
+        );
+      }) ["x86_64-linux"]
+    );
+  };
+}
