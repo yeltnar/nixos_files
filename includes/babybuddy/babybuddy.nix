774f467917737c3870a527110881544b67191025
Drew Brantley <github@andbrant.com>
2025-05-09 14:49:43 -0500

    add babybuddy with backup and restore and secrets via sops

---
+++ includes/babybuddy/babybuddy.nix
@@ -0,0 +1,101 @@
+# man systemd-socket-proxyd
+{
+  # config,
+  pkgs,
+  ...
+}: let
+  name="babybuddy";
+  code_parent_dir="/home/drew/playin";
+  code_dir="${code_parent_dir}/${name}";  
+in {
+  networking.firewall.allowedTCPPorts = [
+    # port for container
+    8000
+  ];
+
+  # enable lingering so service starts before user logs in
+  users.users.drew.linger = true;
+
+  # need to run as system service (not user) because this starts the restore service which must be root 
+  systemd.services."${name}-git-repo" = {
+    path = with pkgs; [
+      git
+    ];
+    description = "${name}-git-repo";
+    wantedBy = [
+      "default.target"
+      "multi-user.target"
+    ];
+    unitConfig = {
+      ConditionPathExists = "!${code_dir}";
+    };
+    script = ''
+      ${pkgs.util-linux}/bin/runuser -u drew -- ${pkgs.bash}/bin/bash -c 'cd ${code_parent_dir}/; git clone https://github.com/yeltnar/${name}';
+    '';
+    serviceConfig = {
+      Type = "oneshot";
+      SyslogIdentifier = "${name}";
+      WorkingDirectory = "${code_parent_dir}";
+    };
+    onSuccess = [
+      # TODO change back to restore
+      "restore.babybuddy.service"
+    ];
+  };
+  
+  # TODO fix this path shiz
+  systemd.user.extraConfig = ''
+    DefaultEnvironment="PATH=/run/current-system/sw/bin"
+  '';
+
+  systemd.user.services."${name}_start" = {
+    path = with pkgs; [
+      podman
+      podman-compose
+    ];
+    wantedBy = [
+      "default.target"
+      "multi-user.target"
+    ];
+    requires = ["podman.service" "podman.socket"];
+    script = ''
+      PATH="$PATH:${pkgs.podman}/bin";
+      ${pkgs.podman-compose}/bin/podman-compose down
+      # ${pkgs.podman-compose}/bin/podman-compose --podman-run-args="--replace --sdnotify=container --pidfile=/tmp/systemd_${name}_podman.pid --gpus=all" up --no-recreate -d
+      ${pkgs.podman-compose}/bin/podman-compose up  -d
+
+      str="Listening on";
+
+      testit(){
+        podman-compose logs | grep "$str" >& /dev/null
+        echo $?
+      }
+
+      while [ 1 -eq `testit` ] ;
+      do 
+        echo loop again
+        sleep 1
+      done
+
+      systemd-notify --ready --status="container up"
+
+    '';
+    unitConfig = {
+      StartLimitInterval = 30;
+      StartLimitBurst = 3;
+      ConditionPathExists = "${code_dir}";
+      RequiresMountsFor = "/run/user/1000/containers";
+    };
+    serviceConfig = {
+      Type = "notify";
+      WorkingDirectory = "${code_dir}";
+      Restart = "always";
+      NotifyAccess = "all";
+      PIDFile = "/tmp/systemd_${name}_podman.pid"; # TODO change pid location 
+      ExecStop = pkgs.writeShellScript "stop-${name}_start" ''
+        PATH="$PATH:${pkgs.podman}/bin";
+        ${pkgs.podman-compose}/bin/podman-compose down
+      '';
+    };
+  };
+}
---
+++ includes/babybuddy/backup_restore.timer.nix
@@ -0,0 +1,80 @@
+{
+  config,
+  pkgs,
+  ...
+}: let
+  unit_id = "babybuddy";
+  scripts = ( import ../helpers/backup_restore_scripts.nix ) { inherit unit_id; };
+  backup_script = scripts.backup_script;
+  restore_script = scripts.restore_script; 
+  backup_env_file = scripts.backup_env_file;
+in {
+
+  sops.secrets."${unit_id}_backup.env" = {
+    owner = "drew";
+    path = backup_env_file;
+  };
+  
+  systemd.timers."backup.${unit_id}" = {
+    wantedBy = [
+      "timers.target"
+    ];
+    timerConfig = {
+      # run service based on how long it last ran 
+      OnUnitInactiveSec = "6h";
+      # start service when timer starts
+      OnActiveSec = "0s";
+      Unit = "backup.${unit_id}.service";
+    };
+  };
+
+
+  systemd.services."backup.${unit_id}" = {
+    environment =
+      config.nix.envVars
+      // {
+        inherit (config.environment.sessionVariables) NIX_PATH;
+        HOME = "/home/drew";
+      }
+      // config.networking.proxy.envVars;
+
+    path = with pkgs; [
+      borgbackup
+    ];
+
+    script = backup_script;
+    unitConfig = {
+      ConditionPathExists = "/home/drew/playin/${unit_id}";
+    };
+    serviceConfig = {
+      WorkingDirectory = "/home/drew/playin/${unit_id}";
+      Type = "oneshot";
+      # User = "drew";
+    };
+  };
+
+  # this should not have a trigger so it only fires after the source code is downloaded 
+  systemd.services."restore.${unit_id}" = {
+    environment =
+      config.nix.envVars
+      // {
+        inherit (config.environment.sessionVariables) NIX_PATH;
+        HOME = "/home/drew";
+      }
+      // config.networking.proxy.envVars;
+
+    path = with pkgs; [
+      borgbackup
+    ];
+    script = restore_script;
+    serviceConfig = {
+      WorkingDirectory = "/home/drew/playin/${unit_id}";
+      Type = "oneshot";
+      # User = "drew";
+      ExecStartPost = "systemctl --user -M drew@ start babybuddy_start.service";
+    };
+    # unitConfig = {
+    #   ConditionPathExists = "/home/drew/playin/${unit_id}";
+    # };
+  };
+}
