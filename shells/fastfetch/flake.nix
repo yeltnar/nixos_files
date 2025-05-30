02ee0ea3e536079915cfd5884e52e6d8f238f19f
drew <github@andbrant.com>
2025-05-30 13:39:38 -0500

    fastfetch enix

---
+++ shells/fastfetch/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "fastfetch";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
+  };
+
+  outputs = { self, nixpkgs } @ args: { 
+    devShells = builtins.listToAttrs ( 
+      map ( system: {
+        name = "${system}";
+        value = (
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              config = {
+                allowUnfree = true;
+              };
+            };
+            # config = self.config;
+          in {
+            default = pkgs.mkShell {
+              packages = with pkgs; [ fastfetch ];
+              shellHook= ''
+                fastfetch; exit; 
+              '';
+            };
+          }
+        );
+      }) ["x86_64-linux" "aarch64-darwin"]
+    );
+  };
+}
