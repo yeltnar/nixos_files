94b4ae5857f9429d21e6acf8278596a45c372ff5
drew <github@andbrant.com>
2025-06-16 15:52:51 -0500

    fix? cosimc issue

--- includes/desktop/cosmic.nix
+++ includes/desktop/cosmic.nix
@@ -9,6 +9,10 @@ let
 in {
 
   config = lib.mkIf ( "cosmic" == desktop_environment ) {
+
+    services.geoclue2.enable = true;
+    services.geoclue2.enableDemoAgent = false;
+
   # Enable the Cosmic Desktop Environment.
     services = {
       displayManager.cosmic-greeter.enable = true;
