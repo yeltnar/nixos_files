afe045c7cde93ae0ec140563fd844934c356fd70
drew <github@andbrant.com>
2025-07-21 19:59:37 -0500

    mor hyperland

--- includes/desktop/hyprland.nix
+++ includes/desktop/hyprland.nix
@@ -25,9 +25,27 @@ in {
       pkgs.waybar
     ];
 
-    services.xserver.displayManager.sddm = {
-      enable = true;
-      wayland.enable = true;
+    programs.hyprlock.enable = true;
+    security.pam.services.hyprlock = {};
+
+    services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        # 'command' tells greetd which greeter to use and what to launch afterwards.
+        # We're using agreety, and telling it to execute Hyprland directly.
+        # command = "${pkgs.greetd.greetd}/bin/agreety --cmd ${pkgs.hyprland}/bin/hyprland";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd ${pkgs.hyprland}/bin/hyprland";
+        user = "greeter"; # Or any user you want greetd to run the greeter as
+                          # This user doesn't need to exist as a login user,
+                          # greetd handles the actual user login.
+      };
     };
+  };
+
+environment.etc."greetd/environments".text = ''
+    bash
+    hyprland
+  '';
   };
 }
