6a16f710d88e6534f1b046f4d802c3b6a2b312f2
drew <github@andbrant.com>
2025-07-28 13:45:55 -0500

    Squashed commit of the following:
    commit 070547e4737ad17f1e6466924c1b425445406e1a
    Author: drew <github@andbrant.com>
    Date:   Mon Jul 28 13:34:48 2025 -0500
    
        move hypr directory to this working tree; link to this dir when build nix
    
    commit d4d56074c9efa45a39eb7fa04e5b64bc8f9d5f86
    Author: drew <github@andbrant.com>
    Date:   Sun Jul 27 11:46:08 2025 -0500
    
        greetd tuigreet time format
    
    commit 807b1a40d1641cc5f8a38380ba2266896fe3de39
    Author: drew <github@andbrant.com>
    Date:   Sat Jul 26 22:44:31 2025 -0500
    
        moving to uwsm from systemd
    
    commit 6cdb571be676814f991a281a7a3f73ba62887cf1
    Author: drew <github@andbrant.com>
    Date:   Sat Jul 26 16:56:36 2025 -0500
    
        greeter clean up
    
    commit ee51d4ebcf6d6cfab81a9f21d5d0817048df5dbf
    Author: drew <github@andbrant.com>
    Date:   Sat Jul 26 16:56:22 2025 -0500
    
        screen sharing setting?
    
    commit d5e397c8718add328342e3b412961a6851da0dd1
    Author: Drew Brantley <github@andbrant.com>
    Date:   Sat Jul 26 14:56:29 2025 -0500
    
        changes to load config files from their locations
    
    commit bf40c33d9da1088404cd84ced5b0867177c3507c
    Author: Drew Brantley <github@andbrant.com>
    Date:   Fri Jul 25 23:04:52 2025 -0500
    
        todos
    
    commit e8896f666b98b28acf995dc70b70848cc79473f4
    Author: drew <github@andbrant.com>
    Date:   Sat Jul 26 12:27:33 2025 -0500
    
        switch to uwsp
    
    commit 2810ddcefba3b0699c1fd352d041aaec69c9493e
    Author: Drew Brantley <github@andbrant.com>
    Date:   Fri Jul 25 22:16:24 2025 -0500
    
        brightness ctl for dispaly
    
    commit 79374ba1b6f121a629dba80296d6a467d80ce452
    Author: drew <github@andbrant.com>
    Date:   Fri Jul 25 16:46:38 2025 -0500
    
        add key ring for hyprland
    
    commit 790d1e3327b6cf9ef3001bbc440b6cefed798e95
    Author: drew <github@andbrant.com>
    Date:   Fri Jul 25 13:52:22 2025 -0500
    
        mor settings apps
    
    commit 682da65d549c306402ace5983f598cd33d6fd453
    Author: drew <github@andbrant.com>
    Date:   Thu Jul 24 13:56:06 2025 -0500
    
        add mor needed packages
    
    commit 47107664b63f2ec6ea337ab227250d4b8b06af67
    Author: drew <github@andbrant.com>
    Date:   Thu Jul 24 10:27:22 2025 -0500
    
        fix it up some: wallapaper, needed apps, app launcher
    
    commit ee66438126211534301dcf34afd6c48333b4418d
    Author: Andrew Brantley <github@andbrant.com>
    Date:   Tue Jul 22 09:51:05 2025 -0500
    
        add hyprspace?
    
    commit 301f12d22998c16ddb068db898ddc9badb8c953f
    Author: Andrew Brantley <github@andbrant.com>
    Date:   Tue Jul 22 07:58:59 2025 -0500
    
        add remember user
    
    commit afe045c7cde93ae0ec140563fd844934c356fd70
    Author: drew <github@andbrant.com>
    Date:   Mon Jul 21 19:59:37 2025 -0500
    
        mor hyperland
    
    commit 0a9e112067b44586013e95a52d7a207e927b65ad
    Author: drew <github@andbrant.com>
    Date:   Mon Jul 21 17:38:47 2025 -0500
    
        setup hyprland

---
+++ includes/desktop/hyprland.nix
@@ -0,0 +1,99 @@
+{
+  config,
+    pkgs,
+    lib, 
+    ...
+}:
+let 
+  desktop_environment = config.services.desktop_environment.selection;
+  hyprspaceConfig =''
+    plugin = ${pkgs.hyprlandPlugins.hyprspace}/lib/libhyprspace.so
+  '';
+  hyprexpoConfig =''
+    plugin = ${pkgs.hyprlandPlugins.hyprexpo}/lib/libhyprexpo.so
+  '';
+  wallpaper = pkgs.fetchurl {
+    url = "https://hot.andbrant.com/milkyway+C&H-nix.jpg";
+    sha256 = "sha256-Xzlv420zq3SOcjDJU0mc7Cew9dNql0IvhQcSvTVbziM=";
+  };
+in
+{
+
+  config = lib.mkIf ( "hyprland" == desktop_environment ) {
+
+    environment.sessionVariables.NIXOS_OZONE_WL = "1";
+    # TODO move settings to nix repo
+    programs.hyprland = {
+      enable = true;
+      withUWSM = true;
+      xwayland.enable = true;
+    };
+
+    # screensharing from nixos site # TODO verify
+    xdg.portal = {
+      enable = true;
+      extraPortals = with pkgs; [ xdg-desktop-portal-hyprland ];
+    };
+
+    hardware = {
+      graphics.enable = true;
+      nvidia.modesetting.enable = true;
+    };
+
+    # we need a keyring for some app login to work
+    services.gnome.gnome-keyring.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      hyprlandPlugins.hyprspace
+      hyprlandPlugins.hyprexpo
+      waybar
+      wofi
+      hyprpaper
+      hypridle
+      playerctl
+      brightnessctl
+      wl-clipboard
+      uwsm
+      networkmanagerapplet
+      blueman # start GUI with blueman-manager
+      pavucontrol # audio control
+      adwaita-icon-theme
+    ];
+
+    programs.hyprlock.enable = true;
+    security.pam.services.hyprlock = {};
+
+    services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        # 'command' tells greetd which greeter to use and what to launch afterwards.
+        # We're using agreety, and telling it to execute Hyprland directly.
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --time --time-format \"%b %-d %I:%M:%S\" --cmd \"${pkgs.uwsm}/bin/uwsm start hyprland-uwsm.desktop\"";
+        user = "greeter";
+      };
+    };
+  };
+
+  environment.etc."greetd/environments".text = ''
+    bash
+    hyprland
+  '';
+  
+  environment.etc."hypr/hyprspace".source = "${pkgs.hyprlandPlugins.hyprspace}/lib/libhyprspace.so";
+  environment.etc."hypr/hyprexpo".source = "${pkgs.hyprlandPlugins.hyprexpo}/hyprexpo/lib/libhyprexpo.so";
+  environment.etc."hypr/wallpaper.jpg".source = "${wallpaper}";
+
+  # Mount the Hyprland configuration file
+  environment.etc."hypr/hyprspace.conf".text = hyprspaceConfig;
+  environment.etc."hypr/hyprexpo.conf".text = hyprexpoConfig;
+
+   
+  system.activationScripts.hypr_ln = {
+    text = ''
+      ln -s /home/drew/playin/nixos_files/includes/desktop/hypr /home/drew/.config/hypr
+    '';
+  };
+
+  };
+}
