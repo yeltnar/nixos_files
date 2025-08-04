04fac4fb59a749ea5b26da09e54e7de1ea022b2b
Drew Brantley <drew@drew-mbp.local>
2025-08-04 11:06:41 -0500

    update switch workspace bemenu

--- includes/desktop/hypr/bemenu/switch-workspace.sh
+++ includes/desktop/hypr/bemenu/switch-workspace.sh
@@ -11,22 +11,26 @@ echo $workspace_count
 # we need to add one for some reason
 workspace_count=$((workspace_count+1))
 
-# exit
-
-# Prompt the user for a workspace number using wofi's dmenu mode
-# -d: Use dmenu mode
-# -p "Prompt Text": Sets the prompt text
-# empty string so there is no dropdown 
-
-# workspace_number=$( echo "$active_workspaces" | wofi -d --lines="$workspace_count" --sort="default" -p "Go to Workspace:")
-
-workspace_number=$(hyprctl workspaces | awk '/^[a-z]/{print $3}' | sort -g | bemenu --center -W .5 --fn 32)
-
-# Check if the user entered a number and didn't cancel (wofi returns an empty string on cancel)
-if [[ -n "$workspace_number" && "$workspace_number" =~ ^[0-9]+$ ]]; then
-  # Use hyprctl to switch to the specified workspace
-  hyprctl dispatch workspace "$workspace_number"
+prompt=""
+if [[ "move" == "$@" ]]; then
+  prompt="move"
+  action="movetoworkspace"
 else
-  # Optional: Echo a message if the input was invalid or cancelled
-  echo "Invalid input or operation cancelled."
+  prompt="switch"
+  action="workspace"
 fi
+
+workspace_list=$(hyprctl workspaces | awk '/^[a-z]/{print $3}' | sort -g)
+workspace_list=""
+workspace_number=$( echo "$workspace_list" | bemenu -n -p "$prompt" -W .5 --center --fn 32)
+
+if [[ ! -n "$workspace_number" ]]; then
+  # Check if the user entered a number and didn't cancel (wofi returns an empty string on cancel)
+  echo 'no workspace provided'
+elif [[ ! "$workspace_number" =~ ^[0-9]+$ ]]; then
+  # if not match number regrex
+  workspace_number="name:$workspace_number"
+fi
+
+hyprctl dispatch "$action" "$workspace_number"
+
