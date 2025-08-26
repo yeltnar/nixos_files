d90ededf9b12e77dc405246e88474b98ed6e5cbf
drew <github@andbrant.com>
2025-08-26 13:35:58 -0500

    multi arch for homemanager flake

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -15,18 +15,19 @@
     home-manager,
     ...
   }: let
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
+    systems = ["aarch64-linux" "x86_64-linux"];
   in {
-    homeConfigurations."drew" = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-
-      # Specify your home configuration modules here, for example,
-      # the path to your home.nix.
-      modules = [./home.nix];
-
-      # Optionally use extraSpecialArgs
-      # to pass through arguments to home.nix
-    };
+    packages = builtins.listToAttrs ( map ( system: {
+      name = "${system}";
+      value = {
+        homeConfigurations."drew" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [./home.nix];
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+    };}) systems );
   };
 }
