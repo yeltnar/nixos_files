e9ebd5e592f83e130180d6e6c73e9d915d65ccb6
drew <nixosvm@andbrant.com>
2025-09-06 15:40:19 -0500

    Squashed commit of the following:
    commit ce117c755956b00048d6011f4b9c408fac4e4633
    Author: drew <nixosvm@andbrant.com>
    Date:   Sat Sep 6 15:38:26 2025 -0500
    
        neater
    
    commit 5ba1a9fb117ef152abb04283d4861ef0e4164284
    Merge: 1e5d8cc 8749e98
    Author: drew <nixosvm@andbrant.com>
    Date:   Sat Sep 6 15:26:10 2025 -0500
    
        Merge branch 'main' into key-playin
    
    commit 8749e98619e9765c527e5fdd28d8972027f4df5e
    Author: drew <nixosvm@andbrant.com>
    Date:   Sat Sep 6 15:06:51 2025 -0500
    
        use if and reg option
    
    commit 1e5d8cc54aaa292073bc2d0e436cc9dfe07094c8
    Author: drew <nixosvm@andbrant.com>
    Date:   Sat Sep 6 10:56:25 2025 -0500
    
        works again
    
    commit 3aa2419036fe5a612b74c23ec977235351301afb
    Author: drew <nixosvm@andbrant.com>
    Date:   Sat Sep 6 10:55:16 2025 -0500
    
        works
    
    commit ce5c4523f64ce8ec22a8e65b5a697120033372df
    Author: drew <nixosvm@andbrant.com>
    Date:   Thu Sep 4 08:38:06 2025 -0500
    
        update mqtt report ip
    
    commit 12188fffbc235d48fddc31227be67f91410c0427
    Author: drew <nixosvm@andbrant.com>
    Date:   Thu Sep 4 08:37:51 2025 -0500
    
        proxmos - orginize imports; turn off babybuddy

--- includes/example/use_key_in_option.nix
+++ includes/example/use_key_in_option.nix
@@ -11,14 +11,11 @@
     default = null;
   };
 
-  config = lib.mkIf ( config.my.test != null && config.my.test != {} ) {
-
-    environment.systemPackages = ( 
-      lib.attrsets.mapAttrsToList (name: value: pkgs.writeShellScriptBin name "echo \"this is an example of using a key: ${name}\"" ) config.my.test 
-    ) ++ [
-      (pkgs.writeShellScriptBin "fmd3" "echo fmd3")
-    ];
-
-  };
+  config.environment.systemPackages = lib.mkIf ( config.my.test != null && config.my.test != {} ) 
+  (
+    lib.attrsets.mapAttrsToList (name: value: pkgs.writeShellScriptBin name "echo \"this is an example of using a key: ${name}\"" ) config.my.test 
+  )
+  //
+  pkgs.writeShellScriptBin "fmd3" "echo fmd3";
 
 }
