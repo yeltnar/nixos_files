0416c176b0a1162aafa24b8ed76d233c047ec69a
drew <github@andbrant.com>
2025-09-25 12:20:26 -0500

    add some shells

---
+++ shells/cosmic-settings/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "lima vm";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs } @ args: { 
+    devShells = builtins.listToAttrs ( 
+      map ( system: {
+        name = "${system}";
+        value = (
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              config = {
+                allowUnfree = true;
+              };
+            };
+            # config = self.config;
+          in {
+            default = pkgs.mkShell {
+              packages = with pkgs; [ cosmic-settings ];
+              shellHook= ''
+                cosmic-settings
+              '';
+            };
+          }
+        );
+      }) ["x86_64-linux" "aarch64-darwin"]
+    );
+  };
+}
