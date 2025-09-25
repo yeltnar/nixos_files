aa218690b9992998de42aff838b6d4f3b81f99d0
drew <github@andbrant.com>
2025-09-25 15:35:15 -0500

    add llama-cpp options
    Squashed commit of the following:
    
    commit efc4dcbc59b8edad26afd75fff2d372d0681143f
    Author: drew <github@andbrant.com>
    Date:   Thu Sep 25 15:34:41 2025 -0500
    
        clean
    
    commit 91f8e5556905ad5f36d4477c36973fc41d4a84bd
    Author: drew <github@andbrant.com>
    Date:   Thu Sep 25 14:03:29 2025 -0500
    
        fix issue with boot race condition
    
    commit 75db3a8253a0d1811b60f73243c22cb6a6e55ded
    Author: drew <github@andbrant.com>
    Date:   Thu Sep 25 12:55:30 2025 -0500
    
        testing

--- includes/granite-ollama-serverless/granite-ollama-serverless.nix
+++ includes/granite-ollama-serverless/granite-ollama-serverless.nix
@@ -59,10 +59,12 @@ in {
 
   systemd.user.services.granite-ollama_small_serverless = {
     requires = [
+      # "granite-ollama_start_llama-cpp.service"
       "granite-ollama_start_ollama.service"
       "granite-ollama_small_serverless.socket"
     ];
     after = [
+      # "granite-ollama_start_llama-cpp.service"
       "granite-ollama_start_ollama.service"
       "granite-ollama_small_serverless.socket"
     ];
@@ -71,6 +73,43 @@ in {
       ExecStart = "${pkgs.systemd}/lib/systemd/systemd-socket-proxyd --exit-idle-time=180s 127.0.0.1:11434";
     };
   };
+
+
+  systemd.user.sockets.granite-llama-cpp_small_serverless = {
+    requires = [
+      # "network-online.target"
+      "default.target"
+    ]; 
+    wantedBy = [
+      "default.target"
+      "sockets.target"
+      "multi-user.target"
+    ];
+    listenStreams = [
+      # "127.0.0.1:8443"
+      "8082"
+      # "192.168.2.180:9999"
+      # "8080"
+    ];
+  };
+  systemd.user.services.granite-llama-cpp_small_serverless = {
+    requires = [
+      # "granite-llama-cpp_start_llama-cpp.service"
+      "granite-ollama_start_llama-cpp.service"
+      "granite-llama-cpp_small_serverless.socket"
+    ];
+    after = [
+      # "granite-llama-cpp_start_llama-cpp.service"
+      "granite-ollama_start_llama-cpp.service"
+      "granite-llama-cpp_small_serverless.socket"
+    ];
+    serviceConfig = { 
+      ExecStart = "${pkgs.systemd}/lib/systemd/systemd-socket-proxyd --exit-idle-time=180s 127.0.0.1:8081";
+    };
+  };
+
+
+
 
   # serverless socket setup for open-webui, and ollama
   systemd.user.sockets.granite-ollama_serverless = {
@@ -93,11 +132,13 @@ in {
 
   systemd.user.services.granite-ollama_serverless = {
     requires = [
+      # "granite-ollama_start_llama-cpp.service"
       "granite-ollama_start_ollama.service"
       "granite-ollama_start_open-webui.service"
       "granite-ollama_serverless.socket"
     ];
     after = [
+      # "granite-ollama_start_llama-cpp.service"
       "granite-ollama_start_ollama.service"
       "granite-ollama_start_open-webui.service"
       "granite-ollama_serverless.socket"
@@ -135,12 +176,60 @@ in {
     DefaultEnvironment="PATH=/run/current-system/sw/bin"
   '';
 
+  systemd.user.services.granite-ollama_start_llama-cpp = {
+    path = with pkgs; [
+      podman
+      podman-compose
+    ];
+    # for some reason when I have this turned on, podman networking breaks
+    # requires = ["podman.service" "podman.socket"];
+    # WARNING this process can not self re-start, or it will confuse the serverless aspect
+    script = ''
+      PATH="$PATH:${pkgs.podman}/bin";
+      ${pkgs.podman-compose}/bin/podman-compose down llama-cpp
+      ${pkgs.podman-compose}/bin/podman-compose --podman-run-args="--replace --sdnotify=container --pidfile=/tmp/systemd_proxy_llama-cpp_podman.pid --gpus=all" up --no-recreate -d llama-cpp
+
+      podman-compose logs -f llama-cpp 2>&1 | while IFS= read -r line; do
+        # Process the line
+        if [[ "$line" == *"listening on"* ]]; then
+          echo "Found a match: $line"
+          # Take action, e.g., run another command
+          systemd-notify --ready --status="container up"
+          break
+        else
+          echo "x-$line"
+        fi
+      done
+
+    '';
+    # wantedBy = ["multi-user.target"];
+    unitConfig = {
+      StopWhenUnneeded = "yes";
+      StartLimitInterval = 30;
+      StartLimitBurst = 3;
+      ConditionPathExists = "${code_dir}";
+      RequiresMountsFor = "/run/user/1000/containers";
+    };
+    serviceConfig = {
+      Type = "notify";
+      WorkingDirectory = "${code_dir}";
+      Restart = "always";
+      NotifyAccess = "all";
+      PIDFile = "/tmp/systemd_proxy_llama-cpp_podman.pid"; # TODO change pid location 
+      ExecStop = pkgs.writeShellScript "stop-granite-llama-cpp_start_open-webui" ''
+        PATH="$PATH:${pkgs.podman}/bin";
+        ${pkgs.podman-compose}/bin/podman-compose down llama-cpp
+      '';
+    };
+  };
+
   systemd.user.services.granite-ollama_start_ollama = {
     path = with pkgs; [
       podman
       podman-compose
     ];
-    requires = ["podman.service" "podman.socket"];
+    # for some reason when I have this turned on, podman networking breaks
+    # requires = ["podman.service" "podman.socket"];
     # WARNING this process can not self re-start, or it will confuse the serverless aspect
     script = ''
       PATH="$PATH:${pkgs.podman}/bin";
@@ -187,7 +276,8 @@ in {
       podman
       podman-compose
     ];
-    requires = ["podman.service" "podman.socket"];
+    # for some reason when I have this turned on, podman networking breaks
+    # requires = ["podman.service" "podman.socket"];
     # WARNING this process can not self re-start, or it will confuse the serverless aspect
     script = ''
       PATH="$PATH:${pkgs.podman}/bin";
