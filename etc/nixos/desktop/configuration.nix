958e50269816889d633f178235c3afeaf20a7d86
drew <github@andbrant.com>
2025-11-10 14:35:38 -0600

    btrfs assistant for desktop

--- etc/nixos/desktop/configuration.nix
+++ etc/nixos/desktop/configuration.nix
@@ -209,6 +209,7 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+  btrfs-assistant
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
     libnbd 
