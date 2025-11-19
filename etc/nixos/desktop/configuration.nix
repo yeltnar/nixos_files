38de6e8928f3f85ab654c47758ea0047b9b40ea0
drew <github@andbrant.com>
2025-11-19 15:04:49 -0600

    update desktop; small package changes

--- etc/nixos/desktop/configuration.nix
+++ etc/nixos/desktop/configuration.nix
@@ -209,7 +209,7 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  btrfs-assistant
+    btrfs-assistant
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
     libnbd 
@@ -242,7 +242,7 @@ in
 
     # TODO check if it works 
     snapper
-    lima
+    # lima
     ulauncher 
   ];
 
