a7158172c3e10acbb0bfb7235fd0ec6c32e5464d
drew <github@andbrant.com>
2025-11-19 15:28:02 -0600

    easyeffects shell

---
+++ shells/easyeffects/flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "EasyEffects audio editor";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=25.05";
+  };
+
+  outputs = { self, nixpkgs } @ args: { 
+    devShells = builtins.listToAttrs ( 
+      map ( system: {
+        name = "${system}";
+        value = (
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              config = {
+                allowUnfree = true;
+              };
+            };
+            # config = self.config;
+          in {
+            default = pkgs.mkShell {
+              packages = with pkgs; [ easyeffects ];
+              shellHook= ''
+                easyeffects ; exit 
+              '';
+            };
+          }
+        );
+      }) ["x86_64-linux"]
+    );
+  };
+}
