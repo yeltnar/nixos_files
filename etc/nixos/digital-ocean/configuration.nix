f954c6c8cf2272e36839afab74a6521f1fb3545e
drew <nixosvm@andbrant.com>
2025-12-10 21:20:31 -0600

    fix up wireguard server... need to validate digital ocean

--- etc/nixos/digital-ocean/configuration.nix
+++ etc/nixos/digital-ocean/configuration.nix
@@ -23,7 +23,6 @@ in {
       ../../../includes/ntfy_server/ntfy-server.nix
       # ../../../includes/ntfy_server/backup_restore.timer.nix
 
-      ../../../includes/wireguard_server/wireguard_server.nix
       # ../../../includes/wireguard_server/backup_restore.timer.nix
 
       ../../../includes/container_ssh/conainer_ssh.nix
@@ -35,13 +34,44 @@ in {
     ];
 
   custom.compose.wireguard_server = {
-    allowedTCPPorts = [
+    allowedUDPPorts = [
       51820
     ];
     files_to_backup="data allocation.txt";
     linger = true;
     use_run_env = false;
     test_string = "All tunnels are now active";
+    backup_restore = false; 
+
+    super_user_clone = true;
+    super_user_start = true;
+
+    super_user_restore = true;
+    super_user_backup_timer = true;
+    super_user_backup = true;
+  };
+  # this is my hack to get it to work... I have not been able to find the correct 'after' to get it to work 
+  systemd.services.restart_wireguard = {
+    script = ''
+    sleep_time=1
+    echo sleeping "$sleep_time" 
+    ${pkgs.coreutils-full}/bin/sleep "$sleep_time"
+    echo slept for "$sleep_time"
+    echo restart starting
+    ${pkgs.systemd}/bin/systemctl restart wireguard_server_start.service
+    echo restart done
+    '';
+    wantedBy = [
+      "default.target"
+      "multi-user.target"
+    ];
+    # requires = ["podman.service" "podman.socket"];
+    after = ["default.target" "nm-online.service" "network-online.target" "firewall.service" "time-set.target"];
+    wants = ["network-online.target"];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true; 
+    };
   };
 
   nix.settings.trusted-users = [leUser];
