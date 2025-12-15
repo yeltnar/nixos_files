dea7f3cf9e06da7f2c45683fb24630efa01436fc
drew <nixosvm@andbrant.com>
2025-12-15 11:06:20 -0600

    generate .sops file with jinja

---
+++ sops_gen/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "jinja2-cli";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+  };
+
+  outputs = { self, nixpkgs } @ args: { 
+    devShells = builtins.listToAttrs (
+      map ( system: {
+        name = "${system}";
+        value = (
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              config = {
+                allowUnfree = true;
+              };
+            };
+            # config = self.config;
+          in {
+            default = pkgs.mkShell {
+              packages = with pkgs; [ jinja2-cli ];
+              shellHook= ''
+                jinja2 template.j2 config.json | yq > ../.sops.yaml
+                exit
+              '';
+            };
+          }
+        );
+      }) ["x86_64-linux" "aarch64-darwin"]
+    );
+  };
+}
