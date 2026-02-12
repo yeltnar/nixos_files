eca3487b8388be1b6fee20cdc9aad37e2381a62c
drew <github@andbrant.com>
2026-02-12 11:43:50 -0600

    permissions for trillium backup/restore

--- includes/triliumnotes/triliumnotes.nix
+++ includes/triliumnotes/triliumnotes.nix
@@ -9,6 +9,13 @@
     allowedTCPPorts = [
       7080
     ];
+
+    super_user_clone = true; # need so restore can start
+    super_user_restore = true;
+    super_user_start = false;
+    super_user_backup_timer = true;
+    super_user_backup = true;
+
     test_string = "Listening on port";
     enable_clone_service = true;
     use_run_env = false;
