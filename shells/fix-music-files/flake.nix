6e6c0a0eb3a63dc20878598b0ec04f231aa4aa42
drew <github@andbrant.com>
2026-04-16 12:20:52 -0500

    fix-music-files shell add check to use current dir

--- shells/fix-music-files/flake.nix
+++ shells/fix-music-files/flake.nix
@@ -32,8 +32,16 @@
                 # -w
                 # -a
                 if [[ -z "$music_dir" ]]; then
-                  echo "music_dir is undefined; exiting"
-                  exit
+                    echo "music_dir is undefined."
+                    read -p "Would you like to use the current directory instead? (y/n): " confirm
+                    
+                    if [[ "$confirm" == "y" || "$confirm" == "Y" ]]; then
+                        music_dir=$(pwd)
+                        echo "Using directory: $music_dir"
+                    else
+                        echo "Exiting."
+                        exit 1
+                    fi
                 fi
                 beet -d "$music_dir" import -m -w -a "$music_dir"
                 exit
