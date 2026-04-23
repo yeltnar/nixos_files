d13ce0dc95fb754fbfc72abfeb0b97abd924f8d7
drew <github@andbrant.com>
2026-04-23 08:43:33 -0500

    updates for start_work

--- etc/nixos/desktop/configuration.nix
+++ etc/nixos/desktop/configuration.nix
@@ -44,6 +44,7 @@ in
       ../../../includes/notify_boot/notify_boot.nix
 
       ../../../includes/webapp/work-chromium.nix
+      ../../../includes/start_work.nix
     ] 
     ++
     lib.fileset.toList (
@@ -212,7 +213,6 @@ in
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    ( import ../../../includes/start_work.nix { inherit pkgs; } )
     btrfs-assistant
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
--- includes/start_work.nix
+++ includes/start_work.nix
@@ -1,8 +1,38 @@
 { pkgs, ... }:
-pkgs.writeShellScriptBin "start_work" ''
-  hyprctl dispatch workspace name:w2
-  bash -c 'work-chromium &' >/dev/null 2>&1
-  sleep 3;
-  hyprctl dispatch workspace name:w1
-  bash -c 'teams-webapp &' >/dev/null 2>&1
-''
+let
+  teams-webapp = pkgs.writeShellScriptBin "teams-webapp" ''
+    # chromium --user-data-dir=/home/drew/.config/teams --profile-directory=Default --app-id=cifhbcnohmdccbgoicgdjpfamggdegmo
+    chromium --user-data-dir=/home/drew/.config/teams --profile-directory=Default --app=https://teams.microsoft.com/v2/
+  '';
+
+
+  start_work = pkgs.writeShellScriptBin "start_work" ''
+
+    # hyprctl dispatch workspace name:w2
+    # bash -c 'work-chromium &' >/dev/null 2>&1
+    # sleep 3;
+    # hyprctl dispatch workspace name:w1
+    # bash -c 'teams-webapp &' >/dev/null 2>&1
+
+    hyprctl dispatch workspace name:w2
+    bash -c 'work-chromium &' >/dev/null 2>&1
+    # Loop until a window is detected in the current workspace
+    while true; do
+        # Check if the active workspace has any windows (clients)
+        if [[ $(hyprctl activeworkspace -j | jq '.windows') -gt 0 ]]; then
+            break
+        fi
+        sleep 0.1
+    done
+    hyprctl dispatch workspace name:w1
+    bash -c '${teams-webapp}/bin/teams-webapp &' >/dev/null 2>&1
+
+  '';
+
+in
+{
+  environment.systemPackages =[
+    teams-webapp
+    start_work
+  ];
+}
