28917944aaa301cc29b142da15f86418ea80aaff
drew <github@andbrant.com>
2026-04-28 12:13:08 -0500

    rustfs switch to helper function

--- shells/rustfs/flake.nix
+++ shells/rustfs/flake.nix
@@ -1,4 +1,3 @@
-
 {
   description = "postman";
 
@@ -11,106 +10,92 @@
       system = "x86_64-linux"; # Adjust to "aarch64-linux" or "aarch64-darwin" if needed
       pkgs = import nixpkgs { inherit system; };
 
-      lib = nixpkgs.lib;
+      # This function holds your logic and accepts overrides
+      mkRustfs = { 
+        use_docker ? false, 
+        storage_port ? "9000", 
+        ui_port ? "9001" 
+      }: 
+      let
+        runner_command = if use_docker then "docker-compose" else "podman-compose";
+        # runner_command = "podman-compose";
 
-      use_docker = false;
-      storage_port = "9000";
-      ui_port = "9001";
+        runner_packages = with pkgs;
+          if use_docker
+          then [ docker docker-compose ]
+          else [ podman podman-compose ];
 
-      runner_command = if use_docker then "docker-compose" else "podman-compose";
-      # runner_command = "podman-compose";
+        my_packages = with pkgs; [
+        ] ++ runner_packages;
 
-      runner_packages = with pkgs;
-        if use_docker
-        then [ docker docker-compose ]
-        else [ podman podman-compose ];
+        container_data_dir = "/home/drew/playin/rustfs/data";
+        
+        compose_file = builtins.toFile "compose.yaml" ''
+          # default login rustfsadmin/rustfsadmin
+          services:
+            rustfs:
+              image: rustfs/rustfs:latest
+              container_name: rustfs
+              ports:
+                - "${storage_port}:9000"
+                - "${ui_port}:9001"
+              volumes:
+                # - rustfs-data:/data
+                - ${container_data_dir}:/data
+              environment:
+                - RUSTFS_ACCESS_KEY=user
+                - RUSTFS_SECRET_KEY=pass
+              stdin_open: true # Corresponds to -i
+              tty: true        # Corresponds to -t
 
-      my_packages = with pkgs; [
-      ] ++ runner_packages;
+          volumes:
+            rustfs-data:
+          '';
 
-      container_data_dir = "/home/drew/playin/rustfs/data";
-      
-      compose_file = builtins.toFile "compose.yaml" ''
-        # default login rustfsadmin/rustfsadmin
-        services:
-          rustfs:
-            image: rustfs/rustfs:latest
-            container_name: rustfs
-            ports:
-              - "${storage_port}:9000"
-              - "${ui_port}:9001"
-            volumes:
-              # - rustfs-data:/data
-              - ${container_data_dir}:/data
-            environment:
-              - RUSTFS_ACCESS_KEY=user
-              - RUSTFS_SECRET_KEY=pass
-            stdin_open: true # Corresponds to -i
-            tty: true        # Corresponds to -t
-
-        volumes:
-          rustfs-data:
-      '';
-
-      data_parent_dir="$HOME/tmp/postman_data/nixpkgs";
-
-      rustfsPkg = pkgs.writeShellApplication {
-        name = "my_app";
-        runtimeInputs = my_packages;
-        text = ''
-          mkdir -p ${container_data_dir}
-          chmod 777 ${container_data_dir}
-          ${runner_command} -f ${compose_file} up
-        '';
+        rustfsPkg = pkgs.writeShellApplication {
+          name = "my_app";
+          runtimeInputs = my_packages;
+          text = ''
+            echo "use_docker: ${if use_docker then "true" else "false"}"
+            echo "storage_port: ${storage_port}"
+            echo "ui_port: ${ui_port}"
+            mkdir -p ${container_data_dir}
+            chmod 777 ${container_data_dir}
+            ${runner_command} -f ${compose_file} up
+          '';
+        };
+      in {
+        pkg = rustfsPkg;
+        app = {
+          type = "app";
+          program = "${rustfsPkg}/bin/my_app";
+        };
       };
 
-      rustfsApp = {
-        type = "app";
-        program = "${rustfsPkg}/bin/my_app";
-      };
-
-      # shell = {
-      #   type = "app";
-      #   program = "${pkgs.writeShellApplication {
-      #     name = "shell";
-      #     runtimeInputs = my_packages;
-      #     text = ''
-      #       exec bash
-      #     '';
-      #   }}/bin/shell";
-      # };
-      #
-      print = {
-        type = "app";
-        program = "${pkgs.writeShellScript "s" ''
-        nix flake show --json ${self}
-        ''}";
-      };
+      # Generate the default versions for this flake
+      defaultOutput = mkRustfs {};
 
       pwd = {
         type = "app";
         program = "${pkgs.writeShellScript "s" ''
-        echo pkg ${rustfsPkg}
-        echo compose_file ${compose_file}
+        echo pkg ${defaultOutput.pkg}
         ''}";
       };
 
     in
     {
-      # packages.${system}.default = runApp;
+      # Expose the builder function so other flakes can call it
+      lib.mkRustfs = mkRustfs;
 
-      # Allows you to use 'nix run .'
       apps.${system} = {
-        default = rustfsApp;
-        rustfs = rustfsApp;
-        # shell = shell;
+        default = defaultOutput.app;
+        rustfs = defaultOutput.app;
         pwd = pwd;
       };
 
       packages.${system} = {
-        default = rustfsPkg;
-        rustfsPkg = rustfsPkg;
+        default = defaultOutput.pkg;
+        rustfsPkg = defaultOutput.pkg;
       };
     };
 }
-
