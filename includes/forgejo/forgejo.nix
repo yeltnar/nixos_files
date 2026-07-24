b47ceceb21c6929db56e896d01f4389baf30502a
pi coding agent <pi@andbrant.com>
2026-07-24 17:01:14 +0000

    Extract forgejo compose definition

--- includes/forgejo/forgejo.nix
+++ includes/forgejo/forgejo.nix
@@ -1,5 +1,33 @@
 # man systemd-socket-proxyd
 { ... }:
+let
+  forgejoCompose = ''
+    services:
+      forgejo:
+        image: codeberg.org/forgejo/forgejo:15
+        container_name: forgejo
+        restart: unless-stopped
+
+        environment:
+          TZ: America/Chicago
+          USER_UID: "1000"
+          USER_GID: "1000"
+
+          FORGEJO__database__DB_TYPE: sqlite3
+          FORGEJO__server__DOMAIN: localhost
+          FORGEJO__server__ROOT_URL: http://localhost:3630/
+          FORGEJO__server__HTTP_PORT: "3630"
+          FORGEJO__server__SSH_DOMAIN: localhost
+          FORGEJO__server__SSH_PORT: "2222"
+
+        ports:
+          - "3630:3630"
+          - "2222:22"
+
+        volumes:
+          - ./forgejo/data:/data
+  '';
+in
 {
   imports = [
     ../helpers/compose-systemd.nix
@@ -11,32 +39,7 @@
       3630
       2222
     ];
-    compose_file = ''
-      services:
-        forgejo:
-          image: codeberg.org/forgejo/forgejo:15
-          container_name: forgejo
-          restart: unless-stopped
-
-          environment:
-            TZ: America/Chicago
-            USER_UID: "1000"
-            USER_GID: "1000"
-
-            FORGEJO__database__DB_TYPE: sqlite3
-            FORGEJO__server__DOMAIN: localhost
-            FORGEJO__server__ROOT_URL: https://forgejo.h.lan/
-            FORGEJO__server__HTTP_PORT: "3630"
-            FORGEJO__server__SSH_DOMAIN: localhost
-            FORGEJO__server__SSH_PORT: "2222"
-
-          ports:
-            - "3630:3630"
-            - "2222:22"
-
-          volumes:
-            - ./forgejo/data:/data
-    '';
+    compose_file = forgejoCompose;
     files_to_backup="forgejo/data";
     use_run_env = false;
     linger = true;
